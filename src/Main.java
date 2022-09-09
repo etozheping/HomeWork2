fd1f3056117453bdb960db16c96e5e9af610a922
etozheping <krashsamara@gmail.com>
2022-09-10 01:03:31 +0400

    Решение задачи №1.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        //1
+        //12
         var dog = 8;
         System.out.println(dog);
         var cat = 3.6;
