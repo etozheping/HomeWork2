375268c0c035dabb552d009336eeb2ecd15fff2d
etozheping <krashsamara@gmail.com>
2022-09-10 01:10:23 +0400

    Решение задачи №2.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        //
+        //Решение первой задачи
         var dog = 8;
         System.out.println(dog);
         var cat = 3.6;
         System.out.println(cat);
         var paper = 763789;
         System.out.println(paper);
+        //Решение второй задачи
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
     }
 }
