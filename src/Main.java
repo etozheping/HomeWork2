444cf5def1c75fc9e26cd8755f4c163c828827f2
etozheping <krashsamara@gmail.com>
2022-09-10 15:49:41 +0400

    Решение задачи №3.

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,26 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         //Решение первой задачи
-        var dog = 8;
+        var dog = 8.0;
         System.out.println(dog);
         var cat = 3.6;
         System.out.println(cat);
         var paper = 763789;
         System.out.println(paper);
         //Решение второй задачи
-        dog = dog + 4;
-        System.out.println(dog);
-        cat = cat + 4;
-        System.out.println(cat);
-        paper = paper + 4;
-        System.out.println(paper);
+        //dog = dog + 4.0;
+        //System.out.println(dog);
+       // cat = cat + 4.0;
+        //System.out.println(cat);
+       // paper = paper + 4;
+       // System.out.println(paper);
+        //Решение третьей задачи
+            dog = dog - 3.5;
+            System.out.println(dog);
+            cat = cat - 1.6;
+            System.out.println(cat);
+            paper = paper - 7639;
+            System.out.println(paper);
 
     }
 }
